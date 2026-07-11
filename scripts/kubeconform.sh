2b04fd1dcdb9110ff597fe57d2eab49c03256980
Liana <liana@lianas.org>
2026-07-11 09:18:43 -0500

    fix: kubeconform

--- scripts/kubeconform.sh
+++ scripts/kubeconform.sh
@@ -34,7 +34,7 @@ echo "=== Validating kustomizations in ${KUBERNETES_DIR}/flux ==="
 find "${KUBERNETES_DIR}/flux" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
   do
     echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
-    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
+    kubectl kustomize "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
       kubeconform "${kubeconform_args[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
@@ -45,7 +45,7 @@ echo "=== Validating kustomizations in ${KUBERNETES_DIR}/apps ==="
 find "${KUBERNETES_DIR}/apps" -type f -name $kustomize_config -print0 | while IFS= read -r -d $'\0' file;
   do
     echo "=== Validating kustomizations in ${file/%$kustomize_config} ==="
-    kustomize build "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
+    kubectl kustomize "${file/%$kustomize_config}" "${kustomize_args[@]}" | \
       kubeconform "${kubeconform_args[@]}"
     if [[ ${PIPESTATUS[0]} != 0 ]]; then
       exit 1
